ea5f4ac7a0fefce2c2f0a7934117d859ae0d0bef
Zoraiz-khan <146815965+Zoraiz-khan@users.noreply.github.com>
2023-10-22 20:44:40 +0500

    Update airdistance.c

--- airdistance.c
+++ airdistance.c
@@ -14,6 +14,27 @@ double sphericallawofcosines(double lat1, double lon1, double lat2, double lon2)
 
     double distance = acos(sin(lat1rad)*sin(lat2rad)*cos(lat1rad)+cos(lat2rad)*cos(lon2rad-lon1rad)*earth_radius);
     return distance;
+    int main()
+{
+    double lat1,lat2,lon1,lon2;
+    printf("Enter the latitude and longitude of the origin:\n");
+    scanf("lf",&lat1);
+    scanf("lf",&lon1);
+    printf("Enter the latitude and longitude of the destnation:\n");
+    scanf("lf",&lat2);
+    scanf("lf",&lon2);
+    double distance = sphericallawofcosines(lat1,lat2,lon1,lon2);
+
+    printf("\nlocation distance\n");
+    printf("=====================\n");
+    printf("origin:   (%.6lf,%.6lf)\n",lat1,lon1);
+    printf("destnation:  (%.6lf,%.6lf)\n",lat2,lon2);
+    printf("air distance is %.6lf kms\n",distance);
+
+    return 0;
+
+
+}
 
 }
 
